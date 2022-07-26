6f7e414d8045f9eaf49103d86f4393b3d5165324
Eric Zhang <ekzhang1@gmail.com>
2022-07-25 20:43:03 -0400

    Implement interop.allocator with Redis APIs

--- src/interop.zig
+++ src/interop.zig
@@ -1,5 +1,8 @@
 //! Interop helpers for the C redismodule header file.
 
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
 const rm = @import("vendor/redismodule.zig");
 
 /// An error set that can be returned from Redis operations.
@@ -24,3 +27,95 @@ pub fn RedisCommand(comptime func: fn (*rm.RedisModuleCtx, []*rm.RedisModuleStri
         }
     }.command;
 }
+
+/// A memory allocator that uses the `RedisModule_*` memory allocation functions.
+pub const allocator = Allocator{
+    .ptr = undefined,
+    .vtable = &allocator_vtable,
+};
+const allocator_vtable = Allocator.VTable{
+    .alloc = RedisAllocator.alloc,
+    .resize = RedisAllocator.resize,
+    .free = RedisAllocator.free,
+};
+
+/// Based on the implementation of `std.heap.c_allocator`.
+const RedisAllocator = struct {
+    fn getHeader(ptr: [*]u8) *[*]u8 {
+        return @intToPtr(*[*]u8, @ptrToInt(ptr) - @sizeOf(usize));
+    }
+
+    fn alignedAlloc(len: usize, alignment: usize) ?[*]u8 {
+        // Thin wrapper around regular malloc, overallocate to account for
+        // alignment padding and store the orignal malloc()'ed pointer before
+        // the aligned address.
+        var unaligned_ptr = @ptrCast([*]u8, rm.RedisModule_Alloc(len + alignment - 1 + @sizeOf(usize)) orelse return null);
+        const unaligned_addr = @ptrToInt(unaligned_ptr);
+        const aligned_addr = std.mem.alignForward(unaligned_addr + @sizeOf(usize), alignment);
+        var aligned_ptr = unaligned_ptr + (aligned_addr - unaligned_addr);
+        getHeader(aligned_ptr).* = unaligned_ptr;
+
+        return aligned_ptr;
+    }
+
+    fn alignedFree(ptr: [*]u8) void {
+        const unaligned_ptr = getHeader(ptr).*;
+        rm.RedisModule_Free(unaligned_ptr);
+    }
+
+    fn alignedAllocSize(ptr: [*]u8) usize {
+        const unaligned_ptr = getHeader(ptr).*;
+        const delta = @ptrToInt(ptr) - @ptrToInt(unaligned_ptr);
+        return rm.RedisModule_MallocSize(unaligned_ptr) - delta;
+    }
+
+    fn alloc(
+        _: *anyopaque,
+        len: usize,
+        alignment: u29,
+        len_align: u29,
+        return_address: usize,
+    ) Allocator.Error![]u8 {
+        _ = return_address;
+        std.debug.assert(len > 0);
+        std.debug.assert(std.math.isPowerOfTwo(alignment));
+
+        var ptr = alignedAlloc(len, alignment) orelse return Allocator.Error.OutOfMemory;
+        if (len_align == 0) {
+            return ptr[0..len];
+        }
+        const full_len = alignedAllocSize(ptr);
+        std.debug.assert(full_len >= len);
+        return ptr[0..std.mem.alignBackwardAnyAlign(full_len, len_align)];
+    }
+
+    fn resize(
+        _: *anyopaque,
+        buf: []u8,
+        buf_align: u29,
+        new_len: usize,
+        len_align: u29,
+        return_address: usize,
+    ) ?usize {
+        _ = buf_align;
+        _ = return_address;
+        if (new_len <= buf.len) {
+            return std.mem.alignAllocLen(buf.len, new_len, len_align);
+        }
+        const full_len = alignedAllocSize(buf.ptr);
+        if (new_len <= full_len) {
+            return std.mem.alignAllocLen(full_len, new_len, len_align);
+        }
+    }
+
+    fn free(
+        _: *anyopaque,
+        buf: []u8,
+        buf_align: u29,
+        return_address: usize,
+    ) void {
+        _ = buf_align;
+        _ = return_address;
+        alignedFree(buf.ptr);
+    }
+};
