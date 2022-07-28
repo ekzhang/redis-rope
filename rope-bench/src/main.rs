4905434289d44fca1d4cd226e95cb8c2be7306a0
Eric Zhang <ekzhang1@gmail.com>
2022-07-27 21:58:33 -0400

    Fix benchmarks for append_64mb and add str version

--- rope-bench/src/main.rs
+++ rope-bench/src/main.rs
@@ -4,13 +4,12 @@ use std::process::{ExitStatus, Stdio};
 
 use ansi_term::Color::{Blue, Green, Red};
 use ansi_term::Style;
-use anyhow::{ensure, Context, Result};
+use anyhow::{ensure, Result};
 use clap::Parser;
 use indoc::formatdoc;
 use nix::sys::signal::{self, Signal};
 use nix::unistd::Pid;
-use redis::aio::Connection;
-use redis::{AsyncCommands, Client, FromRedisValue};
+use redis::{AsyncCommands, Client, Cmd};
 use tokio::fs;
 use tokio::io::AsyncWriteExt;
 use tokio::process::{Child, Command};
@@ -106,14 +105,14 @@ where
     result
 }
 
-async fn query<T: FromRedisValue>(cmd: &str, conn: &mut Connection) -> Result<T> {
+fn cmd(cmd: &str) -> Cmd {
     let mut iter = cmd.split(' ');
-    let start = iter.next().context("command is empty")?;
+    let start = iter.next().expect("command is empty");
     let mut builder = redis::cmd(start);
     for arg in iter {
         builder.arg(arg);
     }
-    Ok(builder.query_async(conn).await?)
+    builder
 }
 
 async fn basic_ops(client: Client) -> Result<()> {
@@ -129,11 +128,11 @@ async fn basic_ops(client: Client) -> Result<()> {
 async fn rope_len(client: Client) -> Result<()> {
     let conn = &mut client.get_async_connection().await?;
 
-    let result: i64 = query("ROPE.LEN hello", conn).await?;
+    let result: i64 = cmd("ROPE.LEN hello").query_async(conn).await?;
     ensure!(result == 0);
 
     conn.set("hello", "world").await?;
-    let result = query::<i64>("ROPE.LEN hello", conn).await;
+    let result = cmd("ROPE.LEN hello").query_async::<_, i32>(conn).await;
     ensure!(result.is_err());
     ensure!(result.unwrap_err().to_string().contains("WRONGTYPE"));
     conn.del("hello").await?;
@@ -144,12 +143,31 @@ async fn rope_len(client: Client) -> Result<()> {
 async fn append_64mb(client: Client) -> Result<()> {
     let conn = &mut client.get_async_connection().await?;
 
-    for i in 0..4096 {
-        let result: i64 = query("ROPE.APPEND a 1234567890123456", conn).await?;
-        ensure!(result == (i + 1) * 16);
+    let append_cmd = cmd(&format!("ROPE.APPEND a {}", "1234567890123456".repeat(64)));
+    let mut pipe = redis::pipe();
+    for _ in 0..65536 {
+        pipe.add_command(append_cmd.clone()).ignore();
     }
-    let result: i64 = query("ROPE.LEN a", conn).await?;
-    ensure!(result == 65536);
+    pipe.add_command(cmd("ROPE.LEN a"));
+    let (result,): (i64,) = pipe.query_async(conn).await?;
+    ensure!(result == 2_i64.pow(26));
+    conn.del("a").await?;
+
+    Ok(())
+}
+
+async fn append_64mb_str(client: Client) -> Result<()> {
+    let conn = &mut client.get_async_connection().await?;
+
+    let append_cmd = cmd(&format!("APPEND a {}", "1234567890123456".repeat(64)));
+    let mut pipe = redis::pipe();
+    for _ in 0..65536 {
+        pipe.add_command(append_cmd.clone()).ignore();
+    }
+    pipe.add_command(cmd("STRLEN a"));
+    let (result,): (i64,) = pipe.query_async(conn).await?;
+    ensure!(result == 2_i64.pow(26));
+    conn.del("a").await?;
 
     Ok(())
 }
@@ -168,6 +186,7 @@ async fn main() -> Result<()> {
         run_test(&client, basic_ops).await?;
         run_test(&client, rope_len).await?;
         run_test(&client, append_64mb).await?;
+        run_test(&client, append_64mb_str).await?;
         println!("{}", Blue.paint("----- ALL TESTS PASSED -----"));
     }
 
